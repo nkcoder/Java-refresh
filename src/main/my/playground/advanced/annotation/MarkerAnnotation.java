b066fdf40df176caf68a792d5426b095f1370a24
Daniel <daniel5hbs@gmail.com>
2023-09-14 09:59:51 +1000

    Java annotations

--- src/main/my/playground/advanced/annotation/MarkerAnnotation.java
+++ src/main/my/playground/advanced/annotation/MarkerAnnotation.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 1. use @interface for the definition
+ * 2. @Retention: RUNTIME (Annotations are to be recorded in the class file by the compiler and retained by the VM at run time, so they may be read reflectively.)
+ * 3. @Target: TYPE(class/interface/record), METHOD, FIELD, ...
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface MarkerAnnotation {
